d886c7fd099cbc9858d9344e1888dbe0e1f27635
joesvart <joesvartt@gmail.com>
2021-01-28 16:21:19 -0300

    Order and create commits in the code.

--- src/main/java/me/joesvart/turromantico/Turromantico.java
+++ src/main/java/me/joesvart/turromantico/Turromantico.java
@@ -8,6 +8,7 @@ import me.joesvart.turromantico.listeners.MOTDListener;
 import lombok.Getter;
 import lombok.Setter;
 import me.joesvart.turromantico.listeners.MaintenanceListener;
+import me.joesvart.turromantico.listeners.PlayersListener;
 import me.joesvart.turromantico.utils.ConfigHelper;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.plugin.PluginManager;
@@ -43,19 +44,38 @@ public class Turromantico extends Plugin {
     }
 
     public void registerCommands() {
+        /**
+         * Plugin manager
+         * object for register
+         * more easy the commands
+         * and the listeners.
+         */
         PluginManager pluginManager = getProxy().getPluginManager();
 
+        /**
+         * Register the commands.
+         */
         pluginManager.registerCommand(this, new MaintenanceCommand(this));
         pluginManager.registerCommand(this, new MOTDCommand(this));
         pluginManager.registerCommand(this, new HubCommand(this));
     }
 
     public void registerListeners() {
+        /**
+         * Plugin manager
+         * object for register
+         * more easy the commands
+         * and the listeners.
+         */
         PluginManager pluginManager = getProxy().getPluginManager();
 
+        /**
+         * Register the listeners.
+         */
         pluginManager.registerListener(this, new FakePlayerListener(this));
         pluginManager.registerListener(this, new MaintenanceListener(this));
         pluginManager.registerListener(this, new MOTDListener(this));
+        pluginManager.registerListener(this, new PlayersListener(this));
     }
 
     @Override
